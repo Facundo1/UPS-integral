54d63545a9868daa9554c8f8a1d6da2748d9e402
Facundo Loberse <facundosa123@gmail.com>
2019-08-26 21:07:28 -0300

    Batteries and contact page

---
+++ php/enviar.php
@@ -0,0 +1,10 @@
+<?php
+ $destino="facundosa123@gmail.com";
+ $nombre=$_POST["Nombre"];
+ $correo=$_POST["Correo"];
+ $telefono=$_POST["Telefono"];
+ $mensaje=$_POST["Mensaje"];
+ $contenido="Nombre: ".$nombre."\nCorreo: ".$correo. "\Telefono: ".$telefono"\nMensaje: ".$mensaje;
+ mail($destino,"Contacto", $contenido);
+ header("Location:contact.html");
+?>
