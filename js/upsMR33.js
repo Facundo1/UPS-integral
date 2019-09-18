c0ac3109a3017b4cffe03c6d35dd282a8eb04e0a
Facundo Loberse <facundosa123@gmail.com>
2019-09-17 23:56:33 -0300

    UPS 3 Responsive

--- js/upsMR33.js
+++ js/upsMR33.js
@@ -2,7 +2,6 @@
 let ups4 = document.getElementById('ups--4');
 let ups4Little = document.getElementById('ups---4')
 let ups4P1 = document.getElementById('ups--4.1');
-let ups4P2 = document.getElementById('ups--4.2');
 let ups4P3 = document.getElementById('ups--4.3');
 const previousSrc = "img/ups4.jpg";
 //------------------------------------------------//
@@ -13,11 +12,7 @@ function cambiarImg0() {
 function cambiarImg1() {
     ups4.src = ups4P1.src;
 }
-//------------------------------------------------//
-function cambiarImg2() {
-    ups4.src = ups4P2.src;
 
-}
 //------------------------------------------------//
 function cambiarImg3() {
     ups4.src = ups4P3.src;
@@ -25,7 +20,7 @@ function cambiarImg3() {
 //------------------------------------------------//
 ups4Little.onmouseover = cambiarImg0;
 ups4P1.onmouseover = cambiarImg1;
-ups4P2.onmouseover = cambiarImg2;
+
 ups4P3.onmouseover = cambiarImg3;
 
 
